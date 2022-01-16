4c22a7f08b9451481489b1babd1238b627e41bd0
Awais <razhar5214@bths.edu>
2022-01-16 06:32:46 -0500

    issue #1 resolved Trending Button Works!

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -7,29 +7,43 @@ import axios from 'axios';
 export default function SearchField(){
 
     const myAPIkey = "UiZGWrYMCET0AZBfmHq49TPkz6vNFFSb";
-
+    
     const [userInput, setUserInput] = useState("");
     const [gifArray, setGifArray] = useState([]);
+    const trendingApiUrl = `http://api.giphy.com/v1/gifs/trending?api_key=${myAPIkey}`;
+    const randomApiUrl = `http://api.giphy.com/v1/gifs/random?api_key=${myAPIkey}`;
     let apiUrl = "";
 
     const HandleSubmit = (event) =>{
-        setGifArray([]);
         event.preventDefault();
+        setGifArray([]);
+        if(event.target.id === "submit_button"){
+            apiUrl = `http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${myAPIkey}`;
+        }else if(event.target.id === "trending_button"){
+            apiUrl = trendingApiUrl;
+        }else{
+            apiUrl = randomApiUrl;
+        }
       
-        apiUrl = `http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${myAPIkey}`;
-
         fetch(apiUrl)
             .then(res => res.json())
-            .then(dataJson =>setGifArray(dataJson.data));
+            .then(dataJson =>{
+                
+                setGifArray(dataJson.data)
+                
+            })
+
         };
+        
     const renderGifs = gifArray.map((item) => {
-        console.log(item);
+        //console.log(item);
         return (
             <GifCard 
                 {...item}
             />
-            );
+        );
     })
+
     return(
         <main>
         <form className="gif--form">
@@ -40,8 +54,11 @@ export default function SearchField(){
                 value={userInput}
                 onChange={(e => setUserInput(e.target.value))}
             />
-            <input type="submit" className="submit-btn" onClick={HandleSubmit}/>
+            <input type="submit" className="submit-btn" id="submit_button" onClick={HandleSubmit}/>
+            <button className="submit-btn" id="trending_button" onClick={HandleSubmit}>Trending</button>
+            <button className="submit-btn" id="random_button" onClick={HandleSubmit}>Random</button>
         </form>
+        
         <div className="gif-container">
             {renderGifs} 
         </div>
